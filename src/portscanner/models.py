0b859d0700291d0e7a8dad66c5b6f5191f9cc9a2
josthedudeoflife <josh.chou@ses-technologies.com>
2015-07-13 22:02:37 -0400

    added model class PortScanner

--- src/portscanner/models.py
+++ src/portscanner/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Portscanner(models.Model):
+	ip_address = models.IPAddressField()
+	port_number = models.DecimalField()
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+	email = models.EmailField()
+	full_name = models.CharField(max_lenght=120, blank=True, null=True)
+
+	def __unicode__(self):
+		return self.email
+		#figure out what to run next
