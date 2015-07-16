a1ef9d91c078bb4de121a53d2f4c115e320ff418
josthedudeoflife <josh.chou@ses-technologies.com>
2015-07-15 21:06:01 -0400

    added timestamp and port number to admin bar

--- src/portscanner/admin.py
+++ src/portscanner/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+#after adding a model add it here
+from .models import Portscanner
+
+class PortscannerAdmin(admin.ModelAdmin):
+	list_display = ["__unicode__", "port_number", "timestamp"]
+	class Meta:
+		model = Portscanner
+
+admin.site.register(Portscanner, PortscannerAdmin)
--- src/portscanner/models.py
+++ src/portscanner/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Portscanner(models.Model):
-	ip_address = models.GenericIPAddressField()
+	ip_address = models.GenericIPAddressField(blank=False)
 	port_number = models.DecimalField(max_digits=19, decimal_places=10)
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 	email = models.EmailField()
 	full_name = models.CharField(max_length=120, blank=True, null=True)
 
 	def __unicode__(self):
-		return self.email
+		return str(self.ip_address)
 		#figure out what to run next
